e8d200fe6fafd6030b5637209ab96a7dc877560a
Elena Denisova <lenaden@gmail.com>
2022-03-17 15:06:46 -0400

    Repository 'removeItemById' method was reworked

--- src/main/java/ru/netology/storage/Repository.java
+++ src/main/java/ru/netology/storage/Repository.java
@@ -22,43 +22,30 @@ public class Repository {
         System.arraycopy(items, 0, temp, 0, items.length);
         return temp;
     }
+    public void removeItemById(int id) {
+        //if product with given id is not found, do nothing with items array.
+        //These 2 lines also handle the case if repository is empty.
+        if(!isExisting(id))
+            return;
 
-    public boolean removeItemById(int idToRemove) {
-        //Here we return true, if item is removed. We return false if item is not found
-        int removeeIndex = getIndexIfExists(idToRemove);
-        if (removeeIndex == -1) {
-            return false; // Item with such id not found. Do nothing with items array
-        }
 
-        //now we know for sure that we will remove one
-        Product[] modifiedStorage = new Product[items.length - 1];
-
-        //easy cases: removing either from 0-th or from the last position:
-        if (removeeIndex == 0) {
-            System.arraycopy(items, 1, modifiedStorage, 0, items.length - 1);
-        }
-
-        if (removeeIndex == items.length - 1) {
-            System.arraycopy(items, 0, modifiedStorage, 0, items.length - 1);
-        }
-
-        if (removeeIndex > 0 && removeeIndex < items.length - 1) {
-            //need to copy 2 blocks, before removee and after
-            System.arraycopy(items, 0, modifiedStorage, 0, removeeIndex);
-            System.arraycopy(items, removeeIndex + 1, modifiedStorage, removeeIndex, items.length - 1 - removeeIndex);
-        }
-        items = modifiedStorage;
-        return true;
-    }
-
-    private int getIndexIfExists(int idToCheck) {
-        int foundAtIndex = -1;
-        for (int currIndex = 0; currIndex < items.length; currIndex++) {
-            if (items[currIndex].getId() == idToCheck) {
-                foundAtIndex = currIndex;
-                break;
+        //now we know for sure, that we will modify items array:
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
             }
         }
-        return foundAtIndex;
+        items = tmp;
+    }
+    private boolean isExisting(int idToCheck) {
+        for (Product currProduct:items) {
+            if(currProduct.getId() == idToCheck)
+                return true;
+        }
+        return false;
     }
 }
